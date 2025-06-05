eea8fa1a15915bd820d86a6336eaa7600dd51dd2
Hannah Bast <bast@cs.uni-freiburg.de>
2025-06-05 10:56:04 +0200

    Update `spatialjoin-evaluation.py` for SIGSPATIAL'25 submission
    1. Add options `--num-threads`,  `--use-de9im`, `--core-library`,
    and `--spatialjoin-binary`.
    2. Remove cutout option because it is not supported for libgeos
    3. Some minor cleanup
    3. Autoformat with `ruff`

--- scripts/spatialjoin-evaluation.py
+++ scripts/spatialjoin-evaluation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # PYTHON_ARGCOMPLETE_OK
 
-# Copyright 2024, University of Freiburg,
+# Copyright 2024 - 2025, University of Freiburg
 # Chair of Algorithms and Data Structures
 # Author: Hannah Bast <bast@cs.uni-freiburg.de>
 
@@ -52,56 +52,78 @@ def compute(args: argparse.Namespace):
     """
 
     # The six options and a representative letter for each.
-    all_options = [('b', '--no-box-ids'),
-                   ('c', '--no-cutouts'),
-                   ('s', '--no-surface-area'),
-                   ('d', '--no-diag-box'),
-                   ('o', '--no-oriented-envelope'),
-                   ('I', '--use-inner-outer')]
+    #
+    # NOTE: Since May 2025, `spatialjoin` no longer supports cutouts because
+    # they do not work easily with `libgeos`, and we want to compare with
+    # `libgeos`.
+    all_options = [
+        ("b", "--no-box-ids"),
+        # ("c", "--no-cutouts"),
+        ("s", "--no-surface-area"),
+        ("d", "--no-diag-box"),
+        ("o", "--no-oriented-envelope"),
+        ("I", "--use-inner-outer"),
+    ]
     options = [all_options[int(i)] for i in args.option_indexes.split(",")]
 
     # The combinations from `--combinations` as a set.
     combinations = set(args.combinations.split(","))
 
     # Create empty log file (clear it if it already exists).
-    log_file_name = f"{args.basename}.spatialjoin-evaluation.log" \
-        if not args.no_log_file else None
+    log_file_name = (
+        f"{args.basename}.spatialjoin-evaluation.log" if not args.no_log_file else None
+    )
     if log_file_name:
         log_file = open(log_file_name, "w")
         log_file.close()
 
     # Try all combinations of the options.
-    sweep_mode = " --no-fast-sweep-skip" if args.no_fast_sweep_skip else ""
     count = 0
-    for name, combination in all_combinations(options):
+    for name, combination_options in all_combinations(options):
         # Consider only combinations compatible with `args.combinations`.
         if args.combinations != "ALL" and name not in combinations:
             continue
 
+        # Helper lambda that returns the option from the first arguments if
+        # the second argument is `True`, otherwise returns an empty string.
+        def option(name: str, condition: bool) -> str:
+            return f" {name}" if condition else ""
+
         # The command line for this combination.
-        cmd = (f"cat {args.basename}.spatialjoin-input.tsv |"
-               f" spatialjoin{sweep_mode} {combination}")
+        cmd = (
+            f"cat {args.basename}.spatialjoin-input.tsv | "
+            f"{args.spatialjoin_binary}"
+            f" --num-threads {args.num_threads}"
+            f"{option('--de9im', args.use_de9im == 'true')}"
+            f"{option('--no-fast-sweep-skip', args.no_fast_sweep_skip)}"
+            f"{option('--libgeos', args.core_library == 'libgeos')}"
+            f" {combination_options}"
+        )
 
         # Optionally, generate RDF output.
         if args.rdf_output:
-            cmd += (" --contains \" ogc:sfContains \""
-                    " --covers \" ogc:sfCovers \""
-                    " --intersects \" ogc:sfIntersects \""
-                    " --equals \" ogc:sfEquals \""
-                    " --touches \" ogc:sfTouches \""
-                    " --crosses \" ogc:sfCrosses \""
-                    " --overlaps \" ogc:sfOverlaps \""
-                    " --suffix $' .\\n'")
+            cmd += (
+                ' --contains " ogc:sfContains "'
+                ' --covers " ogc:sfCovers "'
+                ' --intersects " ogc:sfIntersects "'
+                ' --equals " ogc:sfEquals "'
+                ' --touches " ogc:sfTouches "'
+                ' --crosses " ogc:sfCrosses "'
+                ' --overlaps " ogc:sfOverlaps "'
+                " --suffix $' .\\n'"
+            )
 
         # Optionally, tee stderr to the log file.
         if log_file_name:
             count += 1
             with open(log_file_name, "a") as log_file:
-                log_file.write(f"\n"
-                               f"### Combination {count}\n"
-                               f"### {name}\n"
-                               f"### {cmd} > /dev/null\n"
-                               f"\n")
+                log_file.write(
+                    f"\n"
+                    f"### Combination {count}\n"
+                    f"### {name}\n"
+                    f"### {cmd} > /dev/null\n"
+                    f"\n"
+                )
             cmd += f" 2> >(tee -a {log_file_name} >&2)"
 
         # Only show the commands?
@@ -111,18 +133,23 @@ def compute(args: argparse.Namespace):
 
         # Run the command and time it.
         start = time.time()
-        result = subprocess.run(cmd, shell=True,
-                                executable="/bin/bash",
-                                stdout=subprocess.DEVNULL,
-                                stderr=subprocess.PIPE)
+        result = subprocess.run(
+            cmd,
+            shell=True,
+            executable="/bin/bash",
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.PIPE,
+        )
         end = time.time()
         total_time = f"{end - start:.3f}"
 
         # If non-zero exit code, print the error message
         if result.returncode != 0:
-            error_message = [line for line in
-                             result.stderr.decode().split("\n")
-                             if " INFO : " not in line]
+            error_message = [
+                line
+                for line in result.stderr.decode().split("\n")
+                if " INFO : " not in line
+            ]
             if len(error_message) > 0:
                 total_time = error_message[0]
             else:
@@ -143,7 +170,6 @@ def compute(args: argparse.Namespace):
 
 
 def read_results(args: argparse.Namespace) -> list[tuple[str, float]]:
-
     """
     Read the results from file `{args.basename}.spatialjoin-evaluation.tsv`.
     """
@@ -197,9 +223,10 @@ def analyze_selected(args: argparse.Namespace):
                 print(f"{name} -> {time / 60:6.1f} min, ", end="")
             else:
                 print(f"{name} -> {time:6.1f} s, ", end="")
-            print(f"{first_time / time:5.2f}x speedup over {first_name}"
-                  f" ({previous_time / time:5.2f}x over {previous_name})")
-
+            print(
+                f"{first_time / time:5.2f}x speedup over {first_name}"
+                f" ({previous_time / time:5.2f}x over {previous_name})"
+            )
 
 
 def analyze_all(args: argparse.Namespace):
@@ -215,11 +242,13 @@ def analyze_all(args: argparse.Namespace):
     min_name, min_duration = sorted_results[0]
     max_name, max_duration = sorted_results[-1]
     med_name, med_duration = sorted_results[len(sorted_results) // 2]
-    print(f"Overall stats  : "
-          f"min {min_duration:.1f}s ({min_name}), "
-          f"max {max_duration:.1f}s ({max_name}), "
-          f"median {med_duration:.1f}s ({med_name}), "
-          f"max/min = {max_duration / min_duration:.1f}x")
+    print(
+        f"Overall stats  : "
+        f"min {min_duration:.1f}s ({min_name}), "
+        f"max {max_duration:.1f}s ({max_name}), "
+        f"median {med_duration:.1f}s ({med_name}), "
+        f"max/min = {max_duration / min_duration:.1f}x"
+    )
     print()
 
     # Best k options, for each fixed k.
@@ -238,16 +267,26 @@ def analyze_all(args: argparse.Namespace):
         if k > 0:
             name, seconds = best[k]
             previous_name, previous_time = best[k - 1]
-            print(f"Best {k} {'option ' if k == 1 else 'options'}: "
-                  f"{name} -> {seconds:6.1f}s, "
-                  f"{baseline_time / seconds:4.1f}x speedup over baseline"
-                  f" ({previous_time / seconds:3.1f}x over previous)")
+            print(
+                f"Best {k} {'option ' if k == 1 else 'options'}: "
+                f"{name} -> {seconds:6.1f}s, "
+                f"{baseline_time / seconds:4.1f}x speedup over baseline"
+                f" ({previous_time / seconds:3.1f}x over previous)"
+            )
 
     print()
 
     # A short human-readable description for each option.
-    descriptions = {0: "box ids", 1: "surface area", 2: "cutouts",
-                    3: "diagonal boxes", 4: "oriented boxes"}
+    #
+    # NOTE: See above, regarding the cutouts option.
+    descriptions = {
+        0: "box ids",
+        1: "surface area",
+        # 2: "cutouts",
+        2: "diagonal boxes",
+        3: "oriented boxes",
+        4: "inner/outer boxes",
+    }
 
     # For each option, compute its maximal and minimal speedup relative
     # to all the other options.
@@ -260,8 +299,9 @@ def analyze_all(args: argparse.Namespace):
         def sort_key(pair):
             name = pair[0]
             i = option_index
-            key = name[:i] + name[i + 1:] + name[i]
+            key = name[:i] + name[i + 1 :] + name[i]
             return key
+
         sorted_results = sorted(results, key=sort_key)
 
         # Iterate over pairs of consecutive items in the sorted list and keep
@@ -275,7 +315,7 @@ def analyze_all(args: argparse.Namespace):
             # Assert that the names differ only in the option at `option_index`
             # and that at `option_index` the difference is only in case.
             assert name1[:option_index] == name2[:option_index]
-            assert name1[option_index + 1:] == name2[option_index + 1:]
+            assert name1[option_index + 1 :] == name2[option_index + 1 :]
             assert name1[option_index].upper() == letter
             assert name2[option_index].upper() == letter
             # Compute the speedup.
@@ -288,58 +328,113 @@ def analyze_all(args: argparse.Namespace):
                 max_speedup = speedup
                 max_speedup_index = i
         # Show the minimal and maximal speedup for this option.
-        min_speedup_names = f"{sorted_results[min_speedup_index + 1][0]} -> " \
-                            f"{sorted_results[min_speedup_index][0]}"
-        max_speedup_names = f"{sorted_results[max_speedup_index + 1][0]} -> " \
-                            f"{sorted_results[max_speedup_index][0]}"
-        print(f"{description:15}: "
-              f"max speedup {max_speedup:4.2f}x ({max_speedup_names}), "
-              f"min speedup {min_speedup:4.2f}x ({min_speedup_names})")
+        min_speedup_names = (
+            f"{sorted_results[min_speedup_index + 1][0]} -> "
+            f"{sorted_results[min_speedup_index][0]}"
+        )
+        max_speedup_names = (
+            f"{sorted_results[max_speedup_index + 1][0]} -> "
+            f"{sorted_results[max_speedup_index][0]}"
+        )
+        print(
+            f"{description:15}: "
+            f"max speedup {max_speedup:4.2f}x ({max_speedup_names}), "
+            f"min speedup {min_speedup:4.2f}x ({min_speedup_names})"
+        )
 
 
 if __name__ == "__main__":
     # Get the basenames of all files of the form `*.spatialjoin-input.tsv` or
     # `*.spatialjoin-evaluation.tsv` in the current directory.
     basenames = set()
-    search_paths = (list(Path(".").glob("*.spatialjoin-input.tsv")) +
-                    list(Path(".").glob("*.spatialjoin-evaluation.tsv")))
+    search_paths = list(Path(".").glob("*.spatialjoin-input.tsv")) + list(
+        Path(".").glob("*.spatialjoin-evaluation.tsv")
+    )
     for p in search_paths:
         basenames.add(re.sub("\\.spatialjoin-[a-z]+\\.tsv*$", "", p.name))
 
+    # By default, assume that the `spatialjoin` repo is checked out, that the
+    # script is run from `/path/to/repo/scripts/` and that the `spatialjoin`
+    # binary is located at `/path/to/repo/build/spatialjoin`.
+    script_path = Path(__file__).resolve()
+    spatialjoin_binary_path = script_path.parent.parent / "build" / "spatialjoin"
+
     # Parse command line arguments.
     parser = argparse.ArgumentParser()
-    parser.add_argument("basename",
-                        help="basename of the dataset").completer = \
-        ChoicesCompleter(list(basenames))
-    parser.add_argument("--only-show-commands",
-                        action="store_true", default=False,
-                        help="Only show the commands that would be executed")
-    parser.add_argument("--analyze",
-                        choices=["total", "parse", "sweep"],
-                        default=None,
-                        help="Analyze the specifed time"
-                        " (reads file produced by previous run)")
-    parser.add_argument("--option-indexes", type=str,
-                        default="0,1,2,3,4,5",
-                        help="Comma-separated list of option indexes "
-                        "(default: 0,1,2,3,4,5)")
-    parser.add_argument("--no-fast-sweep-skip", action="store_true",
-                        default=False,
-                        help="Call spatialjoin with --no-fast-sweep-skip")
-    combinations = ["ALL", "bcsdoi,Bcsdoi,BCsdoi,BCSdoi,BCSDoi,BCSdOi,BCSdoI"]
-    parser.add_argument("--combinations", type=str,
-                        default="ALL",
-                        help="Compute / analyze only these "
-                        "combinations").completer = \
-        ChoicesCompleter(combinations)
-    parser.add_argument("--no-log-file", action="store_true", default=False,
-                        help="No log file "
-                        "(default: <basename>.spatialjoin-evaluation.log)")
-    parser.add_argument("--rdf-output", action="store_true", default=False,
-                        help="Generate RDF output")
-    parser.add_argument("--minutes",
-                        action="store_true", default=False,
-                        help="Show times in minutes instead of seconds")
+    parser.add_argument(
+        "basename", help="basename of the dataset"
+    ).completer = ChoicesCompleter(list(basenames))
+    parser.add_argument(
+        "--num-threads",
+        type=int,
+        default=28,
+        help="Number of threads to use in call of `spatialjoin` (default: 28)",
+    )
+    parser.add_argument(
+        "--use-de9im",
+        choices=["true", "false"],
+        default="true",
+        help="Use DE-9IM for spatial relations (default: true)",
+    )
+    parser.add_argument(
+        "--core-library",
+        choices=["ours", "libgeos"],
+        default="ours",
+        help="Use our core library or `libgeos` (default: `ours`)",
+    )
+    parser.add_argument(
+        "--spatialjoin-binary",
+        type=str,
+        default=str(spatialjoin_binary_path),
+        help="Path to the `spatialjoin` binary, default is "
+        f"{spatialjoin_binary_path}",
+    )
+    parser.add_argument(
+        "--only-show-commands",
+        action="store_true",
+        default=False,
+        help="Only show the commands that would be executed",
+    )
+    parser.add_argument(
+        "--analyze",
+        choices=["total", "parse", "sweep"],
+        default=None,
+        help="Analyze the specifed time" " (reads file produced by previous run)",
+    )
+    parser.add_argument(
+        "--option-indexes",
+        type=str,
+        default="0,1,2,3,4",
+        help="Comma-separated list of option indexes " "(default: 0,1,2,3,4,5)",
+    )
+    parser.add_argument(
+        "--no-fast-sweep-skip",
+        action="store_true",
+        default=False,
+        help="Call spatialjoin with --no-fast-sweep-skip",
+    )
+    combinations = ["ALL", "bsdoi,Bsdoi,BSdoi,BSDoi,BSdOi,BSdoI"]
+    parser.add_argument(
+        "--combinations",
+        type=str,
+        default="ALL",
+        help="Compute / analyze only these " "combinations",
+    ).completer = ChoicesCompleter(combinations)
+    parser.add_argument(
+        "--no-log-file",
+        action="store_true",
+        default=False,
+        help="No log file " "(default: <basename>.spatialjoin-evaluation.log)",
+    )
+    parser.add_argument(
+        "--rdf-output", action="store_true", default=False, help="Generate RDF output"
+    )
+    parser.add_argument(
+        "--minutes",
+        action="store_true",
+        default=False,
+        help="Show times in minutes instead of seconds",
+    )
     argcomplete.autocomplete(parser, always_complete_options="long")
     args = parser.parse_args()
 
