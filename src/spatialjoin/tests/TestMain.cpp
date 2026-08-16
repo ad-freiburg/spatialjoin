7eea929ece3dc168e490b15cc761eba650ba9e17
Patrick Brosi <info@patrickbrosi.de>
2026-08-16 18:51:44 +0200

    typoe

--- src/spatialjoin/tests/TestMain.cpp
+++ src/spatialjoin/tests/TestMain.cpp
@@ -1171,9 +1171,9 @@ int main(int, char**) {
       TEST(std::regex_search(res, pattern1));
       std::regex pattern2("\\$london\\t426521.\\d*\\\tgermany\\$");
       TEST(std::regex_search(res, pattern2));
-      std::regex pattern3("\\$germany\\314975.\\d*\\\teiffel\\$");
+      std::regex pattern3("\\$germany\\t314975.\\d*\\\teiffel\\$");
       TEST(std::regex_search(res, pattern3));
-      std::regex pattern4("\\$eiffel\\314975.\\d*\\\tgermany\\$");
+      std::regex pattern4("\\$eiffel\\t314975.\\d*\\\tgermany\\$");
       TEST(std::regex_search(res, pattern4));
       std::regex pattern5("\\$eiffel\\t340875.\\d*\\\tlondon\\$");
       TEST(std::regex_search(res, pattern5));
